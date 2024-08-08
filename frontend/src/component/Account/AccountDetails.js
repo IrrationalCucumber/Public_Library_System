915ef6535aefc60c8dda5a36e70ec76533d6bfe0
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-08-08 18:31:58 +0800

    Added onChange method

--- frontend/src/component/Account/AccountDetails.js
+++ frontend/src/component/Account/AccountDetails.js
@@ -15,20 +15,55 @@ export function AccountAdd() {
     address: "",
     type: "",
   });
+
+  const handleChange = (e) => {
+    if (e.target.name === "sex") {
+      setAccount((prev) => ({ ...prev, sex: e.target.value }));
+    } else if (e.target.name === "type") {
+      setAccount((prev) => ({ ...prev, type: e.target.value }));
+    } else {
+      setAccount((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    }
+  };
   return (
     <div>
-      <input type="text" placeholder="First name..." name="fname" />
-      <input type="text" placeholder="Last name..." name="lname" />
+      <input
+        type="text"
+        placeholder="First name..."
+        name="fname"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder="Last name..."
+        name="lname"
+        onChange={handleChange}
+      />
       <input type="number" placeholder="Enter age..." name="age" />
-      <select name="sex">
+      <select name="sex" onChange={handleChange}>
         <option value="male">Male</option>
         <option value="female">Female</option>
         <option value="other">Other</option>
       </select>
-      <input type="number" placeholder="Enter contact number..." name="cnum" />
-      <input type="email" placeholder="Enter email address..." name="email" />
-      <input type="text" placeholder="Enter address..." name="address" />
-      <select name="type">
+      <input
+        type="number"
+        placeholder="Enter contact number..."
+        name="cnum"
+        onChange={handleChange}
+      />
+      <input
+        type="email"
+        placeholder="Enter email address..."
+        name="email"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder="Enter address..."
+        name="address"
+        onChange={handleChange}
+      />
+      <select name="type" onChange={handleChange}>
         <option>Customer</option>
         <option>Admin</option>
         <option>Staff</option>
