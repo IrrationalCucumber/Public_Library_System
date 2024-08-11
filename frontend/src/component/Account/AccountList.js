63fa5dcd94a5a8c3bd12f1e0032d8e13588b453c
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-08-11 11:23:54 +0800

    Added ui element for account list

--- frontend/src/component/Account/AccountList.js
+++ frontend/src/component/Account/AccountList.js
@@ -1,16 +1,19 @@
-import { List, ListItem } from "@mui/joy";
+import { Avatar, List, ListDivider, ListItem, ListItemDecorator } from "@mui/joy";
 import React from "react";
 import AccountListItem from "./AccountListItem";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function AccountList() {
   const sampleAccounts = ["name1", "name2", "name3", "name4"];
   return (
     <div>
       {sampleAccounts.map((account, index) => (
-        <List key={index}>
+        <List key={index} size="md" variant="soft">
           <ListItem>
+            <ListItemDecorator><AccountCircleIcon /></ListItemDecorator>
             <AccountListItem id={index} name={account} />
-          </ListItem>
+          </ListItem> 
+          <ListDivider inset="gutter"/>
         </List>
       ))}
     </div>
