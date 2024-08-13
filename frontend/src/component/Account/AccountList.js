2419c6e2458b1c93a92d2a29d77a1f4d27a57594
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-08-13 13:49:14 +0800

    Added action button on Account LIst

--- frontend/src/component/Account/AccountList.js
+++ frontend/src/component/Account/AccountList.js
@@ -1,7 +1,17 @@
-import { Avatar, List, ListDivider, ListItem, ListItemDecorator } from "@mui/joy";
+import {
+  Avatar,
+  Dropdown,
+  List,
+  ListDivider,
+  ListItem,
+  ListItemDecorator,
+  Menu,
+  MenuButton,
+  MenuItem,
+} from "@mui/joy";
 import React from "react";
 import AccountListItem from "./AccountListItem";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 export default function AccountList() {
   const sampleAccounts = ["name1", "name2", "name3", "name4"];
@@ -10,10 +20,22 @@ export default function AccountList() {
       {sampleAccounts.map((account, index) => (
         <List key={index} size="md" variant="soft">
           <ListItem>
-            <ListItemDecorator><AccountCircleIcon /></ListItemDecorator>
+            <ListItemDecorator>
+              <AccountCircleIcon />
+            </ListItemDecorator>
             <AccountListItem id={index} name={account} />
-          </ListItem> 
-          <ListDivider inset="gutter"/>
+            <Dropdown>
+              <MenuButton variant="soft" color="primary">
+                ACTIONS
+              </MenuButton>
+              <Menu variant="soft" color="primary">
+                <MenuItem color="primary">Accept</MenuItem>
+                <MenuItem color="danger">Deny</MenuItem>
+                <MenuItem color="warning">Suspend</MenuItem>
+              </Menu>
+            </Dropdown>
+          </ListItem>
+          <ListDivider inset="gutter" />
         </List>
       ))}
     </div>
